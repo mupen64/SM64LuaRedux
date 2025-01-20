c6c50aa5c911ffa646d11e978e78f28df1481089
Frame <nafoizret@web.de>
2025-01-20 19:22:56 +0100

    order keys for objects saved with persistence.lua

--- src/lib/persistence.lua
+++ src/lib/persistence.lua
@@ -9,9 +9,10 @@
 -- Function export is not portable
 
 --[[ License: MIT (see bottom) ]]
+-- This file was modified to write table values in alphabetical order of their keys
 
 -- Private methods
-local write, writeIndent, writers, refCount;
+local write, writeIndent, writers, refCount, orderedPairs;
 
 persistence =
 {
@@ -48,8 +49,8 @@ persistence =
 		end;
 		file:write("\n} -- multiRefObjects\n");
 		-- Then fill them (this requires all empty multiRefObjects to exist)
-		for obj, idx in pairs(objRefNames) do
-			for k, v in pairs(obj) do
+		for obj, idx in orderedPairs(objRefNames) do
+			for k, v in orderedPairs(obj) do
 				file:write("multiRefObjects["..idx.."][");
 				write(file, k, 0, objRefNames);
 				file:write("] = ");
@@ -144,7 +145,7 @@ writers = {
 			else
 				-- Single use table
 				file:write("{\n");
-				for k, v in pairs(item) do
+				for k, v in orderedPairs(item) do
 					writeIndent(file, level+1);
 					file:write("[");
 					write(file, k, level+1, objRefNames);
@@ -180,6 +181,52 @@ writers = {
 			file:write("nil --[[userdata]]\n");
 		end;
 }
+
+-- orderedPairs implementation, as seen in http://lua-users.org/wiki/SortedIteration
+
+local function __genOrderedIndex( t )
+    local orderedIndex = {}
+    for key in pairs(t) do
+        table.insert( orderedIndex, key )
+    end
+    table.sort( orderedIndex )
+    return orderedIndex
+end
+
+local function orderedNext(t, state)
+    -- Equivalent of the next function, but returns the keys in the alphabetic
+    -- order. We use a temporary ordered key table that is stored in the
+    -- table being iterated.
+
+    local key = nil
+    -- print("orderedNext: state = "..tostring(state) )
+    if state == nil then
+        -- the first time, generate the index
+        t.__orderedIndex = __genOrderedIndex( t )
+        key = t.__orderedIndex[1]
+    else
+        -- fetch the next value
+        for i = 1,table.getn(t.__orderedIndex) do
+            if t.__orderedIndex[i] == state then
+                key = t.__orderedIndex[i+1]
+            end
+        end
+    end
+
+    if key then
+        return key, t[key]
+    end
+
+    -- no more value to return, cleanup
+    t.__orderedIndex = nil
+    return
+end
+
+orderedPairs = function(t)
+    -- Equivalent of the pairs() function on tables. Allows to iterate
+    -- in order
+    return orderedNext, t, nil
+end
 
 return persistence
 
