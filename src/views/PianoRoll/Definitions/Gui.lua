cadd6020508b2a2e202a8050b1ac6919ca94a360
Frame <nafoizret@web.de>
2025-05-30 16:46:06 +0200

    define 'Gui' and 'Tab' base classes

---
+++ src/views/PianoRoll/Definitions/Gui.lua
@@ -0,0 +1,18 @@
+---@diagnostic disable:missing-return
+
+---@class Gui
+local __clsGui = {}
+
+---Allocates UIDs for specific subtype
+---@param enumNext fun(count?: integer): integer A function that will generate a new UID whenever called
+---@return table lookup A table from names to UIDs
+function __clsGui.AllocateUids(enumNext) end
+
+---Renders a specific Gui subtype
+---@param draw any A utility object to streamline draw calls.
+function __clsGui.Render(draw) end
+
+---@class Tab:Gui
+---@field name string The name to display for this tab and the key in the UID table for this tab.
+---@field HelpKey string The key by which to look up the help page text as well as the tab title for this tab.
+local __clsTab = {}
