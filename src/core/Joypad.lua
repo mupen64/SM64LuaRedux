8b4b9f5066e55a441d123e4e0f02b23469286080
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-07-11 22:17:54 +0200

    refactor(Joypad): general cleanup

--- src/core/Joypad.lua
+++ src/core/Joypad.lua
@@ -5,13 +5,16 @@
 --
 
 Joypad = {
+	---@type JoypadInputs
 	input = {},
 }
 
+---Updates the Joypad input state.
 function Joypad.update()
 	Joypad.input = joypad.get(Settings.controller_index)
 end
 
+---Sends the current Joypad input state to the emulator.
 function Joypad.send()
 	joypad.set(Settings.controller_index, Joypad.input)
 end
