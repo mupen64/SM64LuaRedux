63087012102eb3039a63d11bc33dbe593684e947
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-10-13 23:49:07 +0200

    fix override regression

--- src/processors/SemanticWorkflow.lua
+++ src/processors/SemanticWorkflow.lua
@@ -5,22 +5,11 @@
 --
 
 local override = nil
-local last_override_nil = true
-local stored_tas_state = NewTASState()
 
 return {
     transform = {
         process = function(input)
-            last_override = override == nil
             override = CurrentSemanticWorkflowOverride()
-
-            -- Store and restore the TAS state during override.
-            if last_override_nil and override ~= nil then
-                stored_tas_state = ugui.internal.deep_clone(Settings.tas)
-            end
-            if not last_override_nil and override == nil then
-                Settings.tas = ugui.internal.deep_clone(stored_tas_state)
-            end
 
             if override then
                 Settings.tas = ugui.internal.deep_clone(override.tas_state)
