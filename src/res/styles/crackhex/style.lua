ecc6e626458d603dde3e73f1ae23993af0813d4c
Hugo Ramos <hugoramos1466@gmail.com>
2026-03-01 12:44:42 +0100

    feat: update French localization strings

--- src/res/styles/crackhex/style.lua
+++ src/res/styles/crackhex/style.lua
@@ -8,7 +8,6 @@ local theme = get_base_style()
 
 theme.font_name = 'Consolas'
 
-theme.font_size = 9
 theme.background_color = BreitbandGraphics.repeated_to_color(34)
 theme.button.text = {
     [1] = BreitbandGraphics.hex_to_color('#F7A8B8'),
