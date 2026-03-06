f02bfe553ebc373b0e21888b7031e45af8ad55b2
Hugo Ramos <hugoramos1466@gmail.com>
2026-03-06 20:12:21 +0100

    fix: recompute setting list item text dynamically on each draw

--- src/views/InteractionSettings.lua
+++ src/views/InteractionSettings.lua
@@ -11,34 +11,33 @@ local UID = UIDProvider.allocate_once('InteractionSettings', function(enum_next)
     }
 end)
 
-local items = {
-    {
-        text = Locales.str('SETTINGS_INTERACTION_MANUAL_ON_JOYSTICK_INTERACT'),
-        func = function(rect)
-            Settings.enable_manual_on_joystick_interact = ugui.toggle_button({
-                uid = UID.EnableManualOnJoystickInteract,
-                rectangle = rect,
-                is_checked = Settings.enable_manual_on_joystick_interact,
-                text = Locales.str('GENERIC_ON'),
-            })
-        end,
-    },
-    {
-        text = Locales.str('SETTINGS_INTERACTION_LOCK_HOTKEYS_WHEN_CONTROL_ACTIVE'),
-        func = function(rect)
-            Settings.lock_hotkeys_when_control_active = ugui.toggle_button({
-                uid = UID.LockHotkeysWhenControlActive,
-                rectangle = rect,
-                is_checked = Settings.lock_hotkeys_when_control_active,
-                text = Locales.str('GENERIC_ON'),
-            })
-        end,
-    },
-}
-
 return {
-    name = Locales.str('SETTINGS_INTERACTION_TAB_NAME'),
+    name = function() return Locales.str('SETTINGS_INTERACTION_TAB_NAME') end,
     draw = function()
+        local items = {
+            {
+                text = Locales.str('SETTINGS_INTERACTION_MANUAL_ON_JOYSTICK_INTERACT'),
+                func = function(rect)
+                    Settings.enable_manual_on_joystick_interact = ugui.toggle_button({
+                        uid = UID.EnableManualOnJoystickInteract,
+                        rectangle = rect,
+                        is_checked = Settings.enable_manual_on_joystick_interact,
+                        text = Locales.str('GENERIC_ON'),
+                    })
+                end,
+            },
+            {
+                text = Locales.str('SETTINGS_INTERACTION_LOCK_HOTKEYS_WHEN_CONTROL_ACTIVE'),
+                func = function(rect)
+                    Settings.lock_hotkeys_when_control_active = ugui.toggle_button({
+                        uid = UID.LockHotkeysWhenControlActive,
+                        rectangle = rect,
+                        is_checked = Settings.lock_hotkeys_when_control_active,
+                        text = Locales.str('GENERIC_ON'),
+                    })
+                end,
+            },
+        }
         Drawing.setting_list(items, { x = 0, y = 0.1 })
     end,
 }
--- src/views/VarWatchSettings.lua
+++ src/views/VarWatchSettings.lua
@@ -15,38 +15,38 @@ local UID = UIDProvider.allocate_once('VarWatchSettings', function(enum_next)
     }
 end)
 
-local items = {
-    {
-        text = Locales.str('SETTINGS_VARWATCH_ANGLE_FORMAT'),
-        func = function(rect)
-            if ugui.button({
-                    uid = UID.AngleFormat,
-                    rectangle = rect,
-                    text = Settings.format_angles_degrees and Locales.str('SETTINGS_VARWATCH_ANGLE_FORMAT_DEGREE') or Locales.str('SETTINGS_VARWATCH_ANGLE_FORMAT_SHORT'),
-                    tooltip = 'The formatting style for angle variables.\n    Short: Formats angles like signed shorts (0-65535)\n    Degree: Formats angles in degrees (0-360)',
-                }) then
-                Settings.format_angles_degrees = not Settings.format_angles_degrees
-            end
-        end,
-    },
-    {
-        text = Locales.str('SETTINGS_VARWATCH_DECIMAL_POINTS'),
-        func = function(rect)
-            Settings.format_decimal_points = math.abs(ugui.numberbox({
-                uid = UID.DecimalPlaces,
-                rectangle = rect,
-                value = Settings.format_decimal_points,
-                places = 1,
-                tooltip = 'The maximum number of decimal places displayed in numbers.',
-            }))
-        end,
-    },
-}
 local selected_var_index = 1
 
 return {
-    name = Locales.str('SETTINGS_VARWATCH_TAB_NAME'),
+    name = function() return Locales.str('SETTINGS_VARWATCH_TAB_NAME') end,
     draw = function()
+        local items = {
+            {
+                text = Locales.str('SETTINGS_VARWATCH_ANGLE_FORMAT'),
+                func = function(rect)
+                    if ugui.button({
+                            uid = UID.AngleFormat,
+                            rectangle = rect,
+                            text = Settings.format_angles_degrees and Locales.str('SETTINGS_VARWATCH_ANGLE_FORMAT_DEGREE') or Locales.str('SETTINGS_VARWATCH_ANGLE_FORMAT_SHORT'),
+                            tooltip = 'The formatting style for angle variables.\n    Short: Formats angles like signed shorts (0-65535)\n    Degree: Formats angles in degrees (0-360)',
+                        }) then
+                        Settings.format_angles_degrees = not Settings.format_angles_degrees
+                    end
+                end,
+            },
+            {
+                text = Locales.str('SETTINGS_VARWATCH_DECIMAL_POINTS'),
+                func = function(rect)
+                    Settings.format_decimal_points = math.abs(ugui.numberbox({
+                        uid = UID.DecimalPlaces,
+                        rectangle = rect,
+                        value = Settings.format_decimal_points,
+                        places = 1,
+                        tooltip = 'The maximum number of decimal places displayed in numbers.',
+                    }))
+                end,
+            },
+        }
         selected_var_index = ugui.listbox({
             uid = UID.SelectedVar,
             rectangle = grid_rect(0, 0, 8, 8),
