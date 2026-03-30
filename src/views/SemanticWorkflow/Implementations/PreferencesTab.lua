5fe885ad073d479b97122cc21389eaefce01850b
Ramos Hugo <132284948+hugou74130@users.noreply.github.com>
2026-03-30 17:49:22 +0200

    Update src/views/SemanticWorkflow/Implementations/PreferencesTab.lua
    Co-authored-by: Aurumaker <48759429+Aurumaker72@users.noreply.github.com>

--- src/views/SemanticWorkflow/Implementations/PreferencesTab.lua
+++ src/views/SemanticWorkflow/Implementations/PreferencesTab.lua
@@ -8,7 +8,7 @@
 ---@diagnostic disable-next-line: assign-type-mismatch
 local __impl = __impl
 
-__impl.name = function() return Locales.str 'SEMANTIC_WORKFLOW_PREFERENCES_TAB_NAME' end
+__impl.name = function() return Locales.str('SEMANTIC_WORKFLOW_PREFERENCES_TAB_NAME') end
 __impl.help_key = 'PREFERENCES_TAB'
 
 ---@type Gui
